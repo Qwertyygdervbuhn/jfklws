578c7249536153133649fb6e3e97ad02183d5195
Alexander Dm. <dabl21291@gmail.com>
2024-11-06 20:47:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication44/ConsoleApplication44.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+public:
+    int x, y;
+
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    friend ostream& operator<<(ostream& os, const Point& point) {
+        os << "(" << point.x << ", " << point.y << ")";
+        return os;
+    }
+  
+    friend istream& operator>>(istream& is, Point& point) {
+        cout << "Enter X: ";
+        is >> point.x;
+        cout << "Enter Y: ";
+        is >> point.y;
+        return is;
+    }
+};
+
+int main() {
+    Point p;
+    cin >> p;
+    cout << "Point: " << p << endl;
+}
